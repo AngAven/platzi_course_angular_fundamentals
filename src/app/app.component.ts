62f56736ad75f1127895c437ce7fdd32f29027bf
Angel <angel.aven.c@gmail.com>
2023-10-03 17:26:09 -0600

    Uso de el evento scroll y keyUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,35 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'Angelin';
-  age = 89;
+  name = 'Angelin'
+  age = 89
   image = 'https://images.unsplash.com/photo-1682685797229-b2930538da47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80';
-  buttonDisabled = true;
-  max = 100;
+  buttonDisabled = true
+  max = 100
 
   person = {
-    name: 'Angelin 2',
+    name: '',
     age: 20,
     avatar: 'https://images.unsplash.com/photo-1682685797229-b2930538da47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80'
   }
 
   toggleButton(){
-    this.buttonDisabled = !this.buttonDisabled;
+    this.buttonDisabled = !this.buttonDisabled
   }
 
   increaseAge(){
     this.person.age += 1
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement
+    console.log('element => ', element )
+    console.log(element.scrollTop)
+  }
+
+  onKeyup(event: Event){
+    const element = event.target as HTMLInputElement
+    console.log('element.value => ', element.value)
+    this.person.name = element.value
   }
 }
